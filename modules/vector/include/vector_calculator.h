46a4dae64f4951179a0c4d94b6979b5068c6d3e6
MaksimShagov <makcim.shagov@mail.ru>
2021-04-25 12:00:22 +0300

    add tests

---
+++ modules/vector/include/vector_calculator.h
@@ -0,0 +1,28 @@
+// Copyright 2021 Shagov Maksim
+
+#ifndef MODULES_VECTOR_INCLUDE_VECTOR_CALCULATOR_H_
+#define MODULES_VECTOR_INCLUDE_VECTOR_CALCULATOR_H_
+
+#include <string>
+#include "include/vector.h"
+
+class VectorCalculator {
+ public:
+    VectorCalculator();
+
+    std::string operator()(int argc, const char** argv);
+
+ private:
+    void help(const char* appname, const char* message = "");
+    bool validateNumberOfArguments(int argc, const char** argv);
+    std::string message_;
+    typedef struct {
+        Vector v_1;
+        Vector v_2;
+        char operation;
+    } Arguments;
+};
+
+//! @}
+
+#endif  // MODULES_VECTOR_INCLUDE_VECTOR_CALCULATOR_H_
